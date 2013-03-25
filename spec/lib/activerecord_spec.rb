2348f3a4c0dea6f1825ecd990870d209c13388ce
Schuyler Erle <schuyler@nocat.net>
2013-03-25 13:25:55 -0700

    Add rspec and documentation for UUID natural keys.

--- spec/lib/activerecord_spec.rb
+++ spec/lib/activerecord_spec.rb
@@ -185,3 +185,14 @@ describe UuidArticle do
     end
   end
 end
+
+describe UuidArticleWithNaturalKey do
+  let!(:article) { Fabricate :uuid_article_with_natural_key }
+  let!(:id) { article.id }
+  let!(:uuid) { UUIDTools::UUID.sha1_create(UUIDTools::UUID_OID_NAMESPACE, article.title) }
+  subject { article }
+  context 'natural_key' do
+    its(:id) { should == uuid }
+  end
+end
+
