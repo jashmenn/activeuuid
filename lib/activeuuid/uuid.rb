4f20fd23ea09142ffa9f6ad0c000e71febeb9b63
Seyed Razavi <seyed@educationapps.co.uk>
2014-02-21 18:09:28 +0000

    add primary_key check

--- lib/activeuuid/uuid.rb
+++ lib/activeuuid/uuid.rb
@@ -143,7 +143,7 @@ module ActiveUUID
 
     def generate_uuids_if_needed
       primary_key = self.class.primary_key
-      if self.class.columns_hash[primary_key].type == :uuid
+      if primary_key && self.class.columns_hash[primary_key].type == :uuid
         send("#{primary_key}=", create_uuid) unless send("#{primary_key}?")
       end
     end
