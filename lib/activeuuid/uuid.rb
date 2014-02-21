4b2132bd263e91a0220b42b687f50f25087c476b
Seyed Razavi <seyed@educationapps.co.uk>
2014-02-21 18:11:34 +0000

    AR4 changes

--- lib/activeuuid/uuid.rb
+++ lib/activeuuid/uuid.rb
@@ -54,13 +54,13 @@ end
 module Arel
   module Visitors
     class DepthFirst < Arel::Visitors::Visitor
-      def visit_UUIDTools_UUID(o)
+      def visit_UUIDTools_UUID(o, a)
         o.quoted_id
       end
     end
 
     class MySQL < Arel::Visitors::ToSql
-      def visit_UUIDTools_UUID(o)
+      def visit_UUIDTools_UUID(o, a)
         o.quoted_id
       end
     end
