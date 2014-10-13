6d7e1c1ce1b63a90779699571b1c588354487e64
Andre Rodrigues <arodrigues@groupon.com>
2014-10-13 13:46:45 -0700

    Adds support for the JDBC MySql adapter.

--- lib/activeuuid/patches.rb
+++ lib/activeuuid/patches.rb
@@ -39,6 +39,32 @@ module ActiveUUID
       end
     end
 
+    module MysqlJdbcColumn
+      extend ActiveSupport::Concern
+
+      included do
+        # This is a really hacky solution, but it's the only way to support the
+        # MySql JDBC adapter without breaking backwards compatibility.
+        # It would be a lot easier if AR had support for custom defined types.
+        #
+        # Here's the path of execution:
+        # (1) JdbcColumn calls ActiveRecord::ConnectionAdapters::Column super constructor
+        # (2) super constructor calls simplified_type from MysqlJdbcColumn, since it's redefined here
+        # (3) if it's not a uuid, it calls original_simplified_type from ArJdbc::MySQL::Column module
+        # (4)   if there's no match ArJdbc::MySQL::Column calls super (ActiveUUID::Column.simplified_type_with_uuid)
+        # (5)     Since it's no a uuid (see step 3), simplified_type_without_uuid is called,
+        #         which maps to AR::ConnectionAdapters::Column.simplified_type (which has no super call, so we're good)
+        #
+        alias_method :original_simplified_type, :simplified_type
+
+        def simplified_type(field_type)
+          return :uuid if field_type == 'binary(16)' || field_type == 'binary(16,0)'
+          original_simplified_type(field_type)
+        end
+      end
+    end
+
+
     module PostgreSQLColumn
       extend ActiveSupport::Concern
 
@@ -113,8 +139,10 @@ module ActiveUUID
       ActiveRecord::ConnectionAdapters::TableDefinition.send :include, Migrations if defined? ActiveRecord::ConnectionAdapters::TableDefinition
 
       ActiveRecord::ConnectionAdapters::Column.send :include, Column
+      ArJdbc::MySQL::Column.send :include, MysqlJdbcColumn if defined? ArJdbc::MySQL::Column
       ActiveRecord::ConnectionAdapters::PostgreSQLColumn.send :include, PostgreSQLColumn if defined? ActiveRecord::ConnectionAdapters::PostgreSQLColumn
 
+      ActiveRecord::ConnectionAdapters::MysqlAdapter.send :include, Quoting if defined? ActiveRecord::ConnectionAdapters::MysqlAdapter
       ActiveRecord::ConnectionAdapters::Mysql2Adapter.send :include, Quoting if defined? ActiveRecord::ConnectionAdapters::Mysql2Adapter
       ActiveRecord::ConnectionAdapters::SQLite3Adapter.send :include, Quoting if defined? ActiveRecord::ConnectionAdapters::SQLite3Adapter
       ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.send :include, PostgreSQLQuoting if defined? ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
